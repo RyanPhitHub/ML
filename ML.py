3b65e41f2c9de9b4915a8b3417d08de5b7f4f764
RyanPhitHub <99211784+RyanPhitHub@users.noreply.github.com>
2023-05-24 01:33:28 -0400

    Update ML.py

--- ML.py
+++ ML.py
@@ -8,11 +8,10 @@ np.set_printoptions(precision=3, suppress=True)
 
 normalize = layers.Normalization()
 
-
-csv_data = pd.read_csv("https://raw.githubusercontent.com/RyanPhitHub/Ryan-Comp-Eng-Projects/main/test.csv", sep= ',', engine = 'python', header= 0)
-csv_train= csv_data.iloc[:2, :9]
+csv_data = (pd.read_csv("https://raw.githubusercontent.com/RyanPhitHub/Ryan-Comp-Eng-Projects/main/Objective%20Compaitibility%20Quiz%20(Responses)%20-%20Form%20Responses%201(2).csv", sep= ',', engine = 'python', header= 0)).drop(["Score"], axis = 1)
+print(csv_data)
 csv_features = csv_data.copy()
-csv_labels = csv_features.pop(' Score')
+csv_labels = csv_features.pop('Total score')
 
 inputs = {}
 
@@ -22,12 +21,10 @@ for name, column in csv_features.items():
     dtype = tf.string
   else:
     dtype = tf.float32
-
   inputs[name] = tf.keras.Input(shape=(1,), name=name, dtype=dtype)
 
 
-numeric_inputs = {name:input for name,input in inputs.items()
-                  if input.dtype==tf.float32}
+numeric_inputs = {name:input for name,input in inputs.items() if input.dtype==tf.float32}
 
 x = layers.Concatenate()(list(numeric_inputs.values()))
 norm = layers.Normalization()
@@ -36,21 +33,38 @@ all_numeric_inputs = norm(x)
 
 preprocessed_inputs = [all_numeric_inputs]
 
-
-
 for name, input in inputs.items():
   if input.dtype == tf.float32:
     continue
-
   lookup = layers.StringLookup(vocabulary=np.unique(csv_features[name]))
   one_hot = layers.CategoryEncoding(num_tokens=lookup.vocabulary_size(), output_mode = 'one_hot')
   x = lookup(input)
   x = one_hot(x)
   preprocessed_inputs.append(x)
-  print(x)
 
 preprocessed_inputs_cat = layers.Concatenate()(preprocessed_inputs)
 csv_preprocessing = tf.keras.Model(inputs, preprocessed_inputs_cat)
 tf.keras.utils.plot_model(model = csv_preprocessing , rankdir="LR", dpi=72, show_shapes=True)
 
-print(one_hot)
+csv_features_dict = {name: np.array(value) for name, value in csv_features.items()}
+
+features_dict = {name:values[:1] for name, values in csv_features_dict.items()}
+print(features_dict)
+csv_preprocessing(features_dict)
+
+def csv_model(preprocessing_head, inputs):
+  body = tf.keras.Sequential([
+    layers.Dense(64),
+    layers.Dense(1)
+  ])
+
+  preprocessed_inputs = preprocessing_head(inputs)
+  result = body(preprocessed_inputs)
+  model = tf.keras.Model(inputs, result)
+
+  model.compile(loss=tf.keras.losses.MeanSquaredError(),
+                optimizer=tf.keras.optimizers.Adam())
+  return model
+
+csv_model = csv_model(csv_preprocessing, inputs)
+csv_model.fit(x=csv_features_dict, y=csv_labels, epochs=50, validation_split = 0.2)
